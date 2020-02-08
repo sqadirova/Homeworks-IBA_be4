47f16c3cbb926d331a2a48c3c9f020057aa4d51f
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-02-08 11:38:29 +0400

    -- Added age limit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,6 +7,16 @@ public class Hello {
         System.out.println("Hello, what is your name?");
         String name=in.next();
         System.out.printf("Hello, %s!\n",name);
+
+        System.out.printf("How old are you, %s?\n",name);
+        int age=in.nextInt();
+        if(age<18){
+            System.out.println("You can go to the cinema!");
+        }
+        else {
+            System.out.println("You can go to night club!");
+        }
+
         System.out.printf("Nice to meet you, %s!\n ",name);
         System.out.println("Bye!");
 
