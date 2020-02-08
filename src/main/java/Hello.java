188b5a9103ae5f7ad9b072d3488deacb54e69842
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-02-08 12:37:24 +0400

    -- Added array

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello, what is your name?");
-        String name=in.next();
-        System.out.printf("Hello, %s!\n",name);
+//        System.out.println("Hello, what is your name?");
+//        String name=in.next();
+//        System.out.printf("Hello, %s!\n",name);
+//
+//        System.out.printf("How old are you, %s?\n",name);
+//        int age=in.nextInt();
+//        if(age<18){
+//            System.out.println("You can go to the cinema!");
+//        }
+//        else {
+//            System.out.println("You can go to night club!");
+//        }
+//
+//        System.out.printf("Nice to meet you, %s!\n ",name);
+//        System.out.println("Bye!");
 
-        System.out.printf("How old are you, %s?\n",name);
-        int age=in.nextInt();
-        if(age<18){
-            System.out.println("You can go to the cinema!");
+        //= = = =  = = = =  =
+        int max=10;
+//        int r=(int)(Math.random()*(max+1));   //[0..10]
+        int[] array=new int[10];
+        for (int i = 0; i <array.length ; i++) {
+            array[i]=(int)(Math.random()*(max+1));
+            System.out.println(array[i]);
         }
-        else {
-            System.out.println("You can go to night club!");
-        }
-
-        System.out.printf("Nice to meet you, %s!\n ",name);
-        System.out.println("Bye!");
 
     }
 }
