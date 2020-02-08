4a9b70a020848860b6a77bb4f259e54101141b89
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-02-08 10:16:24 +0400

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name=in.next();
+        System.out.println("Hello, "+name+"!");
+        System.out.println("Nice to meet you, "+name+"!");
+        System.out.println("Bye!");
+
+    }
+}
