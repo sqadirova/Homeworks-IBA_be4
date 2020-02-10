ab8b39f7c1a285939b09300dc7f61230a2584fe0
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-02-10 18:43:44 +0400

    --2

--- src/main/java/homework1/hw01_Arrays.java
+++ src/main/java/homework1/hw01_Arrays.java
@@ -5,35 +5,30 @@ import java.util.Scanner;
 
 public class hw01_Arrays {
 
-//    private static int[] addNumberToArray(int number,int len)
-//    {
-//        int[] input_numbers=new int[len];
-//
-//
-//        return input_numbers;
-//    }
-
     public static void main(String[] args) {
-       // int[] yournumber=addNumberToArray();
-        int len_array=0;
+
+        //int len_array=0;
+
         //1. Random number
         int max=100;
         int rd_number=(int)(Math.random()*(max+1));    //[0,100]
 //        System.out.printf("Random number: %d\n",rd_number);
-
-
 
         //2.Enter the name
         Scanner in=new Scanner(System.in);
         System.out.println("What is your name?");
         String name= in.nextLine();
 
+        int i=0;
+        int[] arr=new int[100];
         //3.Compare the number
         System.out.println("Let the game begin!");
         while(true){
             System.out.println("Please, enter the number:");
             int number=in.nextInt();
-            len_array++;
+           // len_array++;
+            arr[i] = number;
+            i++;
 
             if (number<rd_number){
                 System.out.println("Your number is too small. Please, try again.");
@@ -44,20 +39,11 @@ public class hw01_Arrays {
                 System.out.println("Your number is too big. Please, try again.");
             }else {
                 System.out.printf("Congratulations, %s\n", name);
-                int[] guessnumbers=new int[len_array];
-                for (int i = 0; i < guessnumbers.length; i++) {
-                    guessnumbers[i]=number;
-                    System.out.print(guessnumbers[i]+"\t");
-                }
+                System.out.print(arr[i] + "\t");
+//                int[] guessnumbers = new int[len_array];
                 break;
-//                int[] guessnumbers=addNumberToArray(number,len_array);
-//                for (int i = 0; i < guessnumbers.length; i++) {
-//                    guessnumbers[i]=number;
-//                    System.out.print(guessnumbers[i]+"\t");
-//                }
-
-
             }
+
 
         }
 
