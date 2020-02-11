9a8e77cabc2db8c8344cff3b79caae74611677c0
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-02-11 10:55:27 +0400

    --Added StringBuilder

--- src/main/java/warmup_tasks/lesson03/envelope1.java
+++ src/main/java/warmup_tasks/lesson03/envelope1.java
@@ -2,7 +2,7 @@ package warmup_tasks.lesson03;
 
 import java.util.Scanner;
 
-public class envelope {
+public class envelope1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Please, enter the width:");
@@ -10,7 +10,7 @@ public class envelope {
         System.out.print("Please, enter the height:");
         final int height=in.nextInt();
         double tan=((double) width)/height;
-        //rectangle
+
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <=width; j++) {
                 if (i==1||i==height||j==1||j==width||j==(int)(i*tan)||j==(width-(int)(i*tan))){
---
+++ src/main/java/warmup_tasks/lesson03/envelope2.java
@@ -0,0 +1,28 @@
+package warmup_tasks.lesson03;
+
+import java.util.Scanner;
+
+public class envelope2 {
+    private static boolean isShouldBeFilled(int i, int j, int width, int height, double tan) {
+        return i==1||i==height||j==1||j==width||j==(int)(i*tan)||j==(width-(int)(i*tan));
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please, enter the width:");
+        final int width=in.nextInt();
+        System.out.print("Please, enter the height:");
+        final int height=in.nextInt();
+        double tan=((double) width)/height;
+       StringBuilder envelope=new StringBuilder();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <=width; j++) {
+                if (isShouldBeFilled(i, j, width, height, tan)){
+                    envelope.append("*");}
+                else {envelope.append(" ");}
+            }
+            envelope.append("\n");
+
+        }
+        System.out.println(envelope);
+    }
+}
