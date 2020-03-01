42c9c2b1182fd0550e907b015ede220e4d1a255f
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-03-01 17:42:17 +0400

    -- Edited Family

--- src/main/java/homework7/Family.java
+++ src/main/java/homework7/Family.java
@@ -9,7 +9,7 @@ public class Family {
     private Human father;
     private Human[] children;
     private Pet pet;
-    private static int count = 2;
+    private static int count = 0;
 
     public Family() {
     }
@@ -54,31 +54,75 @@ public class Family {
     }
 
 
-    public Human[] addChild(Human child, Human[] children_list) {
-        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(children_list));
-        temp.add(child);
-        count++;
-        return temp.toArray(children_list);
+    //    public Human[] addChild(Human child, Human[] children_list) {
+//        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(children_list));
+//        temp.add(child);
+//        count++;
+//        return temp.toArray(children_list);
+//    }
+
+    public boolean addChild(Human child, Human[] originChildren) {
+        int n = originChildren.length;
+        Human[] newChildren = new Human[n + 1];
+        for (int i = 0; i < n; i++) {
+            newChildren[i] = originChildren[i];
+        }
+        newChildren[n] = child;
+        setChildren(newChildren);
+        return true;
     }
 
-    public Human[] deleteChild_Index(Human[] childrenList, int index) {
-        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(childrenList));
-        temp.remove(index);
-        count--;
-        return temp.toArray(childrenList);
+//    public Human[] deleteChild_Index(Human[] childrenList, int index) {
+//        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(childrenList));
+//        temp.remove(index);
+//        count--;
+//        return temp.toArray(childrenList);
+//    }
+
+    public boolean deleteChild_Index(Human[] originChildren, int index) {
+        if (originChildren == null || index < 0 || index >= originChildren.length) {
+            return false;
+        }
+        Human[] anotherChildren = new Human[originChildren.length - 1];
+
+        for (int i = 0, j = 0; i < originChildren.length; i++) {
+            if (i == index) {
+                continue;
+            }
+            anotherChildren[j++] = originChildren[i];
+        }
+        setChildren(anotherChildren);
+        return true;
     }
 
 
-    public Human[] deleteChild_Obj(Human[] childrenList, Human child) {
-        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(childrenList));
-        temp.remove(child);
-        count--;
-        return temp.toArray(childrenList);
+//    public Human[] deleteChild_Obj(Human[] childrenList, Human child) {
+//        ArrayList<Human> temp = new ArrayList<>(Arrays.asList(childrenList));
+//        temp.remove(child);
+//        count--;
+//        return temp.toArray(childrenList);
+//    }
+
+    public boolean deleteChild_Obj(Human[] originChildren, Human child) {
+        if (originChildren == null || child == null) {
+            return false;
+        }
+        int n = originChildren.length;
+        Human[] anotherChildren = new Human[n - 1];
+        for (int i = 0, k = 0; i < n; i++) {
+            if (!child.equals(originChildren[i])) {
+                anotherChildren[k++] = originChildren[i];
+            }
+        }
+        setChildren(anotherChildren);
+
+        return true;
     }
 
     public int countFamily() {
-        return count;
+        return getChildren().length + 2;
     }
+
 
     @Override
     public String toString() {
