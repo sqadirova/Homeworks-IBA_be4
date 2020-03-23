6a350a8860d7475a023ee3611e6235bebbd4981a
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-03-23 09:30:03 +0400

    -- warmup lesson22 recursion

---
+++ src/main/java/warmup_tasks/lesson22recurs/TheSum.java
@@ -0,0 +1,23 @@
+package warmup_tasks.lesson22recurs;
+
+import java.util.Random;
+
+public class TheSum {
+
+  static int sum(int[] data, int n) {
+    //data[n]+data[n-1]+data[n-2]..
+    if (n == 0) {
+      return data[n];
+    } else {
+      return data[n] + sum(data, n - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] d = new Random().ints(1, 10)
+            .limit(10).toArray();
+    int sum = sum(d, d.length - 1);
+
+    System.out.println(sum);
+  }
+}
