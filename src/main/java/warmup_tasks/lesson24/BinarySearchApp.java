5fe55b57357c2ca10524b8b468c95ff907975693
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-03-27 10:58:45 +0400

    -- lesson 24 binary search

--- src/main/java/warmup_tasks/lesson24/BinarySearchApp.java
+++ src/main/java/warmup_tasks/lesson24/BinarySearchApp.java
@@ -7,16 +7,6 @@ public class BinarySearchApp {
   static int[] data = new Random().ints(0, 10000).limit(1000).toArray();
   static int[] sorted = data.clone();
 
-  // return number of `if` statements
-  static int find_conventional(int[] origin, int number) {
-    throw new RuntimeException();
-  }
-
-  // return number of `if` statements
-  static int find_binary(int[] origin, int number) {
-    throw new RuntimeException();
-  }
-
   static int binarySearch(int[] array,int number) {
     int start = 0;
     int end = array.length - 1;
@@ -45,8 +35,7 @@ public class BinarySearchApp {
 //    System.out.println(Arrays.toString(data));
     System.out.println(Arrays.toString(sorted));
 //    int rnd = (int) (Math.random()*10000);
-//    int count1 = find_conventional(data, rnd);
-//    int count2 = find_binary(sorted, rnd);
+
     int index = binarySearch(sorted, sorted[64]);
     System.out.println(index);
   }
