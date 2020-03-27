d89e29820a486594fbe2f11c48a492c46ceac8bd
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-03-27 11:31:17 +0400

    -- lesson 24 binary search boolean

--- src/main/java/warmup_tasks/lesson24/BinarySearchApp.java
+++ src/main/java/warmup_tasks/lesson24/BinarySearchApp.java
@@ -7,17 +7,18 @@ public class BinarySearchApp {
   static int[] data = new Random().ints(0, 10000).limit(1000).toArray();
   static int[] sorted = data.clone();
 
-  static int binarySearch(int[] array,int number) {
+  static boolean binarySearch(int[] array, int number) {
     int start = 0;
     int end = array.length - 1;
-    int count=0;
+    int count = 0;
 
     while (start <= end) {
-      int mid = (start + end) / 2;
       count++;
+      int mid = (start + end) / 2;
       if (array[mid] == number) {
-        System.out.printf("Count of if: %d\n",count);
-        return  mid;
+        System.out.printf("Count of if: %d\n", count);
+        System.out.printf("The searched number is- %d\n",mid);
+        return true;
       } else if (array[mid] > number) {
         end = mid - 1;
       } else if (array[mid] < number) {
@@ -25,22 +26,19 @@ public class BinarySearchApp {
       }
     }
 
-    return -1;
+    return false;
   }
-
 
 
   public static void main(String[] args) {
     Arrays.sort(sorted);
 //    System.out.println(Arrays.toString(data));
     System.out.println(Arrays.toString(sorted));
-//    int rnd = (int) (Math.random()*10000);
-
-    int index = binarySearch(sorted, sorted[64]);
-    System.out.println(index);
+    int rnd_idx = (int) (Math.random() * 1000);
+    int rnd = sorted[rnd_idx];
+    boolean result = binarySearch(sorted, rnd);
+    System.out.println(result);
   }
-
-
 
 
 }
