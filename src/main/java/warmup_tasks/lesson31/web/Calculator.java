26a9d61ad6ede5ff13da001e29184fc3a4a669c9
Siddiqe Qedirova <sidiqa@inbox.ru>
2020-04-10 11:09:11 +0400

    -- LESSON 31

--- src/main/java/warmup_tasks/lesson31/web/Calculator.java
+++ src/main/java/warmup_tasks/lesson31/web/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator {
   public String doOperation(String x, String y, String op) {
     int p1 = Integer.parseInt(x);
     int p2 = Integer.parseInt(y);
-    op= String.valueOf(p1+p2);
-    return op;
+    if(op.equals("+")){return op=String.valueOf(p1+p2);}
+    else if (op.equals("-")){return op= String.valueOf(p1-p2); }
+    else if (op.equals("*")){return op= String.valueOf(p1*p2); }
+    else if (op.equals("/")){return op= String.valueOf(p1/p2); }
+    else return "Wrong operation";
   }
 }
